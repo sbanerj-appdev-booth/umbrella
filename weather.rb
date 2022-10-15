9f396650443bdd1e4b3759fae586c7c4879cac1b
banerso <sbanerje@chicagobooth.edu>
2022-10-15 21:35:36 +0000

    Twilio_complete

--- weather.rb
+++ weather.rb
@@ -1,8 +1,8 @@
 p "Where are you located?"
 
-user_location = "Houston"
+user_location = gets.chomp
 
-p user_location
+#p user_location
 
 gmaps_key =ENV.fetch("GMAPS_KEY")
 
@@ -14,7 +14,7 @@ require "open-uri"
 
 raw_data = URI.open(gmaps_url).read
 
-p raw_data.split
+#p raw_data.split
 
 require "json"
 
@@ -22,7 +22,7 @@ parsed_data = JSON.parse(raw_data)
 
 
 results_array = parsed_data.fetch("results")
-p results_array
+#p results_array
 
 first_result = results_array.at(0)
 
@@ -34,3 +34,39 @@ longitude = loc.fetch("lng")
 
 p latitude
 p longitude
+
+#weather
+
+dark_sky_key = ENV.fetch("DARK_SKY_KEY")
+
+dark_sky_url = "https://api.darksky.net/forecast/#{dark_sky_key}/#{latitude},#{longitude}"
+
+temp_data = URI.open(dark_sky_url).read
+
+parsed_temp_data = JSON.parse(temp_data)
+
+currently_hash = parsed_temp_data.fetch("currently")
+
+current_temp = currently_hash.fetch("temperature")
+
+puts "It is currently #{current_temp}°F."
+
+#Get your credentials from your Twilio dashboard, or from Canvas if you're using mine
+
+twilio_sid = ENV.fetch("TWILIO_SID")
+twilio_token = ENV.fetch("TWILIO_KEY")
+twilio_sending_number = ENV.fetch("TWILIO_PH_NUM")
+
+require "twilio-ruby"
+# Create an instance of the Twilio Client and authenticate with your API key
+twilio_client = Twilio::REST::Client.new(twilio_sid, twilio_token)
+
+# Craft your SMS as a Hash literal with three keys
+sms_info = {
+  :from => twilio_sending_number,
+  :to => ENV.fetch("MY_PH_NUM"), # Put your own phone number here if you want to see it in action
+  :body => "It's going to rain today — take an umbrella!"
+}
+
+# Send your SMS!
+twilio_client.api.account.messages.create(sms_info)
